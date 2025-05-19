e7c5ecfa1ddaf90c844cfc962f35fd0ddb550f68
Ilimsky <ilim.sarykbaev@gmail.com>
2025-05-19 22:28:57 +0600

    added filter field on the basic list 19.05.25 22.28

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "815dc177bb26c29bf971ed4afa1c3fc7",
+"flutter_bootstrap.js": "9666910ccbc84c25bb9d917c02392e81",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "44ee31dcf6424b0c10eca066b85d0837",
 "/": "44ee31dcf6424b0c10eca066b85d0837",
-"main.dart.js": "75a49573f5b284157eb2859322c812fc",
+"main.dart.js": "0a087f35491033023a5b08284c8f81cc",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can
